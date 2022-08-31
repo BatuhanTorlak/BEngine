9b28174dd91f3a842765690a904a2e516e35aab0
Batuhan Torlak <torlakhanbatu@gmail.com>
2022-08-31 20:41:05 +0300

    2

--- Controls/Mouse.cs
+++ Controls/Mouse.cs
@@ -3,6 +3,22 @@
     using Vectors;
     public static class Mouse
     {
+        public static Vector2Int Position
+        {
+            get => GetPosition();
+            set => SetPosition(value);
+        }
+        public static int x
+        {
+            get => Position.x;
+            set => Position = new Vector2Int(value, Position.y);
+        }
+        public static int y
+        {
+            get => Position.y;
+            set => Position = new Vector2Int(Position.x, value);
+        }
+
         public static void Click(int x = 0, int y = 0)
         {
             Win32Mouse.mouse_event(ControlVaruables.Mouse.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
@@ -17,5 +33,14 @@
 
         public static void SetPosition(int x = 0, int y = 0) => Win32Mouse.SetCursorPos(x, y);
         public static void SetPosition(Vector2Int pos) => Win32Mouse.SetCursorPos(pos.x, pos.y);
+
+        public static Vector2Int GetPosition()
+        {
+            if (Win32Mouse.GetCursorPos(out Vector2Int pos))
+            {
+                return pos;
+            }
+            throw null;
+        }
     }
 }
--- Controls/Win32Mouse.cs
+++ Controls/Win32Mouse.cs
@@ -9,5 +9,9 @@ namespace BEngine.Controls
 
         [DllImport("user32.dll")]
         internal static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("user32.dll", SetLastError = false)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(out Vectors.Vector2Int Position);
     }
 }
